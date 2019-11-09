a44aaed0cfa6f0d7b9feec7508487b64a5ffae10
Vivekasr <hie.vivi.sharma@gmail.com>
2019-11-10 01:23:16 +0530

    sub structure inside

--- maingraphic.py
+++ maingraphic.py
@@ -1,15 +1,16 @@
 import turtle
 
-from submarine import submarineOutline
+from submarine import submarineOutline, submarineInline
 from waves import waveOutline
 from sky import skyOutline
 
 turtle.bgcolor('#506265')
 turtle.speed(0)
+turtle.hideturtle()
 
 skyOutline()
 waveOutline()
 submarineOutline()
+submarineInline()
 
-turtle.hideturtle()
 turtle.exitonclick()
